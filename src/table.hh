b0b488b5a4d3d0a529cf30973ccd69471435aeee
VenkatKunaparaju <venkatkrox@gmail.com>
2022-10-30 16:46:17 -0400

    Test

--- src/table.hh
+++ src/table.hh
@@ -1,13 +1,14 @@
 //Row entries would be written to the heap directly, so no struct
 //traverses through row entries to execute queries
 
-
 //contains structs for table table_information which has linked list of fence posts
 #include <iostream>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+#define MAXSTRINGLEN 32 //Max size for names
+#define ARENA_SIZE 256 //Current increments of size to request from the os
 
 struct fencePost {
     int type; //1 for begin, 0 for end
@@ -15,9 +16,13 @@ struct fencePost {
     fencePost *prev;
 
 };
+struct columnInfo {
+    char name[MAXSTRINGLEN]; //Name for column
+    int size; //Size of datatype in column
+};
 struct tableInformation {
     int N; //N -> Number of columns
-    char *column_information; //N * 40 bytes 
+    columnInfo *columns; //N * 40 bytes 
     fencePost *fenceposts; //First fencepost
 };
 
